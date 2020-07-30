e090fefd4feb091e9efcba8b5f31d40312a282f9
bija510 <error.zxz@gmail.com>
2020-07-29 23:33:10 -0400

    Added --Cucumber-- POC include/feature, include/script/scriptSD & Cucumber_TS

---
+++ Object Repository/Page_Facebook/txt_PhoneNum.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_PhoneNum</name>
+   <tag></tag>
+   <elementGuidId>55e5fcc1-1818-4544-8771-482cd7f54e78</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='u_0_r']
+</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
