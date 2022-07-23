31a2db0f9968f03c9d431692b64b236d3d04f7a0
bija510 <56846045+bija510@users.noreply.github.com>
2022-07-23 12:47:41 -0400

    added some fun for window app testing

--- Scripts/ZTemp/test/Script1591319570961.groovy
+++ Scripts/ZTemp/test/Script1591319570961.groovy
@@ -67,6 +67,112 @@ def static getAlphaNumericString(int length){
 
 	return finalStr
 }
+// 01. Run the bat file
+/*****************************************************************************************
+ This work for running the bat file
+ https://stackoverflow.com/questions/10685893/run-exe-file-in-java-from-file-location
+ 
+String batFilePath = RunConfiguration.getProjectDir()+"/Start WinAppDriver.bat"
+println batFilePath
+runtimeProcess = Runtime.getRuntime().exec("cmd /c start /min cmd.exe /C\""+ batFilePath +"\"");
+
+// runtimeProcess = Runtime.getRuntime().exec("cmd /c start cmd.exe /C\""+"C:\\Users\\bchhetri\\Git\\DemTech_Automation\\Start WinAppDriver.bat"+"\"");
+ int processComplete = runtimeProcess.waitFor();
+ 
+*****************************************************************************************/
+
+// 02. find the cmd.exe or window command line is open or not
+/*****************************************************************************************
+ * For the window locator 
+ * https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk/
+
+import java.io.BufferedReader as BufferedReader
+import java.io.IOException as IOException
+import java.io.InputStreamReader as InputStreamReader
+
+
+boolean anyCmdProgramOpened = isAnyCmdProgramOpened()
+System.out.println('isOpened = ' + anyCmdProgramOpened)
+WebUI.callTestCase(findTestCase('aSetup/Setup_01_StartWinAppDriver'), [:], FailureHandling.STOP_ON_FAILURE)
+static boolean isAnyCmdProgramOpened() {
+    Process p
+    
+    try {
+        p = Runtime.getRuntime().exec((System.getenv('windir') + '\\system32\\') + 'tasklist.exe')
+        String line
+        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()))
+        while ((line = input.readLine()) != null) {
+            System.out.println(line)
+            if (line.startsWith('cmd.exe')) {
+                return true            
+                break
+            }
+        }
+    }
+    catch (IOException e) {
+        e.printStackTrace()
+    }     
+    return false
+}
+***************************************************************************************************/
+
+
+// 03. Lunching the center point application and click on the hamburger menu and click the ballots 1st plus btn
+/**************************************************************************************************
+ * Click hamburger menu and ballots 1st plus btn
+ 
+	WebUI.callTestCase(findTestCase('CenterPoint_Inhouse/TC_01_Login_CenterPoint'), [:], FailureHandling.STOP_ON_FAILURE)	
+	Windows.clickElementOffset(findWindowsObject('CenterPoint/MainPage/100Perc_text_leftTo_burger_menu'), 142, 0) // click Hamburger menu
+	Windows.clickElementOffset(findWindowsObject('CenterPoint/ElectionInformationPage/btn_CopyIdentifiers'), 759, -68) // first ballots 1st plus btn
+ **************************************************************************************************/
+
+// 04. Take screenshot on failure
+/***********************************************************************************
+ 	
+	Windows.startApplicationWithTitle(GlobalVariable.centerPoint_url, 'CenterPoint')
+	String actHeader = Windows.getText(findWindowsObject('CenterPoint/Login/lbl_welcomeToDemTech'))
+	Windows.verifyMatch(actHeader, 'Welcome to DemTech CenterPoint', false)
+
+
+	String screenshotLocation = RunConfiguration.getProjectDir()+ "/test.png"
+	WindowsActionHelper.create(WindowsDriverFactory.getWindowsSession()).takeScreenshot(screenshotLocation)
+	
+	//Windows.clickElementOffset(findWindowsObject('CenterPoint/MainPage/100Perc_text_leftTo_burger_menu'), 142, 0) // first plus btn
+
+ ***********************************************************************************/
+ 
+
+// 05. Upload file
+/****************************************************************
+	Windows.click(findWindowsObject('EpollTab/MainDashboard/btn_loadFromFile'))
+	
+	Windows.delay(3)
+	String fileUploadPath =  RunConfiguration.getProjectDir() + '/Data/FilesForUpload/Default Clone Clone_2022_07.pollbook'
+	fileUploadPath = fileUploadPath.replace('/', '\\')
+	
+	StringSelection ss = new StringSelection(fileUploadPath);
+	//Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+	java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+	clipboard.setContents(ss, null);
+	Robot robot = new Robot();
+	robot.keyPress(KeyEvent.VK_DELETE);
+	robot.keyRelease(KeyEvent.VK_DELETE);
+	Windows.delay(3)
+	robot.keyPress(KeyEvent.VK_ENTER);
+	robot.keyRelease(KeyEvent.VK_ENTER);
+	robot.keyPress(KeyEvent.VK_CONTROL);
+	robot.keyPress(KeyEvent.VK_V);
+	robot.keyRelease(KeyEvent.VK_V);
+	robot.keyRelease(KeyEvent.VK_CONTROL);
+	Windows.delay(3)
+	robot.keyPress(KeyEvent.VK_ENTER);
+	robot.keyRelease(KeyEvent.VK_ENTER);
+	Windows.delay(3)
+
+*********************************************************************/
+
+
+
 
 
 
