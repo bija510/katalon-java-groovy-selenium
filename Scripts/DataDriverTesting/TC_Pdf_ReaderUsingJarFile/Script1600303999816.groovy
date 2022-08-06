010020322446ece03df178ec1fb70471354df36e
bija chh <error.zxz@gmail.com>
2022-08-06 12:59:49 -0400

    Update Pdf reader

--- Scripts/DataDriverTesting/TC_Pdf_ReaderUsingJarFile/Script1600303999816.groovy
+++ Scripts/DataDriverTesting/TC_Pdf_ReaderUsingJarFile/Script1600303999816.groovy
@@ -4,6 +4,7 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.util.PDFTextStripper
 import org.testng.Assert
 import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.util.KeywordUtil
 /**************************************************************************************
 	1. We need two jar file// First put in build.gradle without opening katalon studio
 	2. Then CMD in the Location and Run:- gradle katalonCopyDependencies
@@ -11,22 +12,53 @@ import com.kms.katalon.core.configuration.RunConfiguration
 	4. Apache PDFBox  » 1.8.1 https://mvnrepository.com/artifact/org.apache.pdfbox/pdfbox
 **************************************************************************************/
 
-String filePath = RunConfiguration.getProjectDir() + "/Data Files All/PdfReader/test_text2.pdf"
+String filePath = RunConfiguration.getProjectDir() + "/Data Files All/PdfReader/test_text1.pdf"
 
-File file = new File(filePath)
-FileInputStream fis = new FileInputStream(file)
+KeywordUtil.logInfo( "Page Count  = ${get_pageCount(filePath)}" )
+KeywordUtil.logInfo( get_all_text(filePath) )
 
-PDFParser parser = new PDFParser(fis)
-parser.parse()
+Assert.assertTrue( get_all_text(filePath).contains("Review of the Literature") )
 
-COSDocument cosDoc = parser.getDocument()
-PDDocument pdDoc = new PDDocument(cosDoc)
+Assert.assertTrue( get_all_text(filePath).contains("dsdsfdsf") )
 
-PDFTextStripper strip = new PDFTextStripper()
-String data = strip.getText(pdDoc)
 
-println data.trim() // this will remove the white space
-Assert.assertTrue(data.contains("Like other schools around the nation"))
+public String get_all_text(String filePath){
+	File file = new File(filePath)
+	FileInputStream fis = new FileInputStream(file)
+	
+	PDFParser parser = new PDFParser(fis)
+	parser.parse()
+	
+	COSDocument cosDoc = parser.getDocument()
+	PDDocument pdDoc = new PDDocument(cosDoc)
+	
+	PDFTextStripper strip = new PDFTextStripper()
+	String data = strip.getText(pdDoc)
+	
+	println data.trim() // this will remove the white space
+	Assert.assertTrue(data.contains("Like other schools around the nation"))
+	return data
+}
+
+public String get_pageCount(String filePath){
+	File file = new File(filePath)
+	FileInputStream fis = new FileInputStream(file)
+	
+	PDFParser parser = new PDFParser(fis)
+	parser.parse()
+	
+	COSDocument cosDoc = parser.getDocument()
+	PDDocument pdDoc = new PDDocument(cosDoc)
+	int pageCount = pdDoc.getNumberOfPages()
+	String pc = pageCount.toString()
+	return pc
+}
+
+
+
+
+
+
 
 
 
